3b79d01dbb4c1b4f9e77138bf1a40534343d24dd
Khazimla <missmahomana@gmail.com>
2023-05-29 23:51:20 +0200

    changes

---
+++ index.js
@@ -0,0 +1,26 @@
+// Function to convert Celsius to Fahrenheit
+function celsiusToFahrenheit(celsius) {
+    let fahren = (celsius * 9) / 5 + 32;
+    return fahren +"F";
+  }
+
+  // Function to convert Fahrenheit to Celsius
+  function fahrenheitToCelsius(fahrenheit) {
+    let cel = ((fahrenheit - 32) * 5) / 9;
+    return cel + "C";
+  }
+
+  // Prompt the user for input
+  let temperature = parseFloat(prompt("Enter the temperature:"));
+  let conversionType = prompt("Choose the conversion type: (CelsiustoFahrenheit or FahrenheittoCelsius)");
+
+
+  if (conversionType === "Fahrenheit") {
+   alert(celsiusToFahrenheit(temperature))
+
+  } else if (conversionType === "Celsius") {
+    alert(fahrenheitToCelsius(temperature))
+
+  } else {
+    alert("Invalid conversion type.");
+  };
